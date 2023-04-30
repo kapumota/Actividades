809078896664d5dd569374ab86faceafabd1a58b
kapumota <121532729+kapumota@users.noreply.github.com>
2023-04-30 09:47:03 -0500

    Update Rectangle.java

--- TDD-4/TDD-4/src/main/java/shapes/Rectangle.java
+++ TDD-4/TDD-4/src/main/java/shapes/Rectangle.java
@@ -1,3 +1,12 @@
 package shapes;
 
-// Completa
+public class Rectangle{
+    private final int width;
+    private final int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+}
