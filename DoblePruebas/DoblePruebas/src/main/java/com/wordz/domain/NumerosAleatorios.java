86e8f2a147fb4a69f539436a3f4e33c1bfabb7ee
kapumota <claraa@uni.edu.pe>
2023-06-25 10:00:15 -0500

    Actualizamos codigo

--- DoblePruebas/DoblePruebas/src/main/java/com/wordz/domain/NumerosAleatorios.java
+++ DoblePruebas/DoblePruebas/src/main/java/com/wordz/domain/NumerosAleatorios.java
@@ -1,6 +1,6 @@
 package com.wordz.domain;
 
-public interface NumerosAleatorios {
+public interface NumerosAleatorios {//uso de la funcion dada en ejemplos
     int next(int upperBoundInclusive);
 }
 
