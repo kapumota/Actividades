285511ea7e42f70b30a6846fe9f49e32e39375bf
kapumota <121532729+kapumota@users.noreply.github.com>
2023-06-25 09:51:15 -0500

    DoblePruebas

--- DoblePruebas/DoblePruebas/src/main/java/ejemplos/LanzamientoDados.java
+++ DoblePruebas/DoblePruebas/src/main/java/ejemplos/LanzamientoDados.java
@@ -3,9 +3,10 @@ import java.util.random.RandomGenerator;
 public class LanzamientoDados {
     private final int NUMERO_LADOS = 6;
 
-    //private final RandomGenerator rnd = RandomGenerator.getDefault();
-
-    // Completa
+   private final NumerosAleatorios rnd;
+    public LanzamientoDados(NumerosAleatorios r){
+        this.rnd = r;
+    }
     public String asText() {
         int lanzado = rnd.nextInt(NUMERO_LADOS) + 1;
 
