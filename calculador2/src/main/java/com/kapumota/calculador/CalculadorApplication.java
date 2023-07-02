3b8a92beea8b4262a52a151c1f8e89d34836f9ad
kapumota <claraa@uni.edu.pe>
2023-07-01 19:16:39 -0500

    Un ejemplo de uso

---
+++ calculador2/src/main/java/com/kapumota/calculador/CalculadorApplication.java
@@ -0,0 +1,14 @@
+package com.kapumota.calculador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadorApplication {
+    //private static final String constant = "constant";
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadorApplication.class, args);
+	}
+
+}
